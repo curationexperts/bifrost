91f7df29f3fd9338da43fc27dc5903d9386e625f
Justin Coyne <justin@curationexperts.com>
2013-07-02 16:05:53 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BifrSt::Application.config.secret_key_base = '339414301bc1ceaaf37f4bfa843b3b93caddde325a77c08722e83d0a8170ffddc462f780bbe427cac29fd677755e935a2d03e8a76409087b5df2d45ecaae2c4c'
